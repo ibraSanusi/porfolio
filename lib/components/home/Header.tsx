deb79325308e09616f521206d07df0bd2ae0bdf7
ibraSanusi <isanu227@gmail.com>
2024-07-15 15:50:12 +0200

    refactored i guess

--- lib/components/home/Header.tsx
+++ lib/components/home/Header.tsx
@@ -18,24 +18,13 @@ export default function Header() {
       ref={headerRef}
       className="w-full flex p-8 fixed top-0 left-0 z-[999]"
     >
-      <nav className="flex flex-row items-center gap-20 py-2 px-4 rounded-[50px] m-auto bg-opacity-50 bg-[#D9D9D9]">
+      <nav className="flex flex-row items-center gap-10 py-2 px-4 rounded-[50px] m-auto bg-opacity-50 bg-[#D9D9D9]">
         <NavLink href={'#hero'}>Inicio</NavLink>
         <NavLink href={'#experience'}>Experiencia</NavLink>
         <NavLink href={'#projects'}>Proyectos</NavLink>
         <NavLink href={'#about'}>Sobre mí</NavLink>
         <NavLink href={'mailto:ibra.sanusi.ayo@gmail.com'}>Contacto</NavLink>
       </nav>
-      {/* <div className="flex items-center flex-row gap-4">
-        <Link href={'https://github.com/ibraSanusi'} target="_blank">
-          <Github className="size-12" />
-        </Link>
-        <Link
-          href={'https://www.linkedin.com/in/ibrahim-ayodeji-sanusi-0208112a7/'}
-          target="_blank"
-        >
-          <LinkedIn className="size-10" />
-        </Link>
-      </div> */}
     </header>
   )
 }
---
+++ lib/components/home/SectionContainer.tsx
@@ -0,0 +1,78 @@
+'use client'
+
+import { useHeader } from '@lib/hooks/useHeader'
+import React, { ReactNode, useEffect, useRef } from 'react'
+
+interface Props {
+  id: string | undefined
+  className?: string | undefined
+  children: ReactNode
+}
+
+export default function SectionContainer({
+  id,
+  children,
+  className,
+  ...props
+}: Props) {
+  const sectionsRef = useRef<HTMLElement[]>([])
+  const headerRef = useHeader()
+
+  const options = {
+    root: null,
+    rootMargin: '-350px',
+    threshold: 0.4,
+  }
+
+  const callback: IntersectionObserverCallback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        if (!headerRef?.current) return
+
+        const header = headerRef.current
+
+        const navLinks = header.querySelectorAll('nav a')
+        navLinks.forEach((link) => {
+          if (link.getAttribute('href') === `#${entry.target.id}`) {
+            header.classList.add('headerBlur')
+            link.classList.add('headerIntersected')
+          } else {
+            header.classList.add('headerBlur')
+            link.classList.remove('headerIntersected')
+          }
+        })
+      }
+    })
+  }
+
+  // Marca la seccion de la pagina que se esta viendo en el header
+  // Si se esta en proyectos se remarca proyectos en el header
+  useEffect(() => {
+    const observer = new IntersectionObserver(callback, options)
+    sectionsRef.current.forEach((sectionRef) => {
+      observer.observe(sectionRef)
+    })
+
+    return () => {
+      observer.disconnect()
+    }
+  }, [])
+
+  const addSectionRef = (el: HTMLElement | null) => {
+    if (el && !sectionsRef.current.includes(el)) {
+      sectionsRef.current.push(el)
+    }
+  }
+
+  return (
+    <section
+      id={id}
+      data-section={id}
+      ref={addSectionRef}
+      className={`container scroll-m-[204px] ${className}`}
+      {...props}
+    >
+      {children}
+    </section>
+  )
+}
